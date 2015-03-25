75a0254d6b687883b7e5ebee6cabe390dfea3f3c
mhviet <hviet0906@gmail.com>
2015-03-25 17:50:12 +0700

    25Mar2015 - Getting mz / intensity

--- agilentImportCpp.cpp
+++ agilentImportCpp.cpp
@@ -3,7 +3,7 @@
 using namespace Rcpp;
 using namespace std;
 
-#define HEADERSIZE 1440 * 4
+#define HEADERSIZE 5758
 #define MZSIZE 16
 #define INTSIZE 16
 #define SCANSIZE 16
@@ -28,10 +28,22 @@ int16_t read16_be(istream& stream)
         (b[0] << 8) );
 }
 
+float encodeMZ(int16_t mz){
+  return((float)(mz / 20));
+}
+
+int encodeIntensity(int16_t intensity){
+  int shiftNumber = (intensity >> 13) * 3;
+  return(intensity << shiftNumber);
+}
+
 // [[Rcpp::export]]
 List agilentImportCpp(string filename){
   List agilent;
   vector<int> fixedPattern;
+  vector<int> counts;
+  vector<int> mz;
+  vector<int> intensity;
   
   ifstream fbin (filename.c_str(), ios::binary | ios::in);
   if (!fbin) {
@@ -43,16 +55,37 @@ List agilentImportCpp(string filename){
   fbin.seekg (0, ios::beg);
   fbin.seekg(HEADERSIZE * sizeof(char));
   
-  int i = 0;
+  int count = -1;
+  int scanIndex = 0;
+  vector<int> scanMZ;
+  vector<int> scanIntensity;
   while(!fbin.eof()){
-    if(i > 10)
+    for(int i = 0; i < 11; i++)
+      fixedPattern.push_back(read16_be(fbin));
+    count = read16_be(fbin);
+    counts.push_back(count);
+    // Highest peak in scan
+    read16_be(fbin);
+    read16_be(fbin);
+    
+    // Get spectrometry data
+    for(int j = 0; j < count; j++){
+      scanMZ.push_back(encodeMZ(read16_be(fbin)));
+      scanIntensity.push_back(encodeIntensity(read16_be(fbin)));
+    }
+    reverse(scanMZ.begin(), scanMZ.end());
+    mz.push_back(scanMZ);
+    reverse(scanIntensity.begin(), scanIntensity.end());
+    intensity.push_back(scanIntensity);
+    scanIndex++;
+    if(scanIndex > 8000)
       break;
-    fixedPattern.push_back(i);
-    cout << fixedPattern.size() << " " << read16_be(fbin) << "\n";
-    i++;
   }
   cout << "\n";
   agilent["fixed_pattern"] = fixedPattern;
+  agilent["counts"] = counts;
+  agilent["mz"] = mz;
+  agilent["intensity"] = intensity;
   
   fbin.close();
   return agilent;
